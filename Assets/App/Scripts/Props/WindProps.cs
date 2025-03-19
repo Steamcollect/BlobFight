bf0da8b601eff1c66be6626aefd613603e3dc100
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-03-19 14:59:58 +0100

    Fix: WindZone

--- Assets/App/Scripts/Props/WindProps.cs
+++ Assets/App/Scripts/Props/WindProps.cs
@@ -1,14 +1,24 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//[Serializable]
+//public struct RbDelay
+//{
+//    public Rigidbody2D rb2D;
+//    public float delay;
+//    public bool canActiveForce;
+//}
 public class WindProps : GameProps
 {
     [Header("Settings")]
     [SerializeField] float windForce;
+    //[SerializeField] float windForceDelay;
     bool isLaunched = false;
 
     //[Header("References")]
     List<Rigidbody2D> rbs = new();
-
+    //List<RbDelay> rbDelays = new();
     //[Space(10)]
     // RSO
     // RSF
@@ -28,7 +38,8 @@ public class WindProps : GameProps
 
         if(rbs.Count > 0)
         {
-            foreach(var rbs in rbs)
+            //StartCoroutine(DelayAddForce());
+            foreach (var rbs in rbs)
             {
                 rbs.AddForce(transform.up * windForce);
             }
@@ -39,6 +50,11 @@ public class WindProps : GameProps
     {
         if(collision.TryGetComponent(out Rigidbody2D rb))
         {
+            //RbDelay rbDelay = new();
+            //rbDelay.rb2D = rb;
+            //rbDelay.delay = windForceDelay;
+            //rbDelay.canActiveForce = false;
+            //rbDelays.Add(rbDelay);
             rbs.Add(rb);
         }
     }
@@ -49,7 +65,10 @@ public class WindProps : GameProps
             rbs.Remove(rb);
         }
     }
-
+    //IEnumerator DelayAddForce()
+    //{
+    //    yield return new WaitForSeconds(windForceDelay);
+    //}
     public void SetWindForce(float windForceChange)
     {
         windForce = windForceChange;
