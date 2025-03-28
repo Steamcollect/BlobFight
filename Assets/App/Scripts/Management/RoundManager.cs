916433853ac3d1a919f3c0b41d2002a113aab8da
Furnok <killian.furno@orange.fr>
2025-03-29 00:11:25 +0100

    Fix: Code Clean Up

--- Assets/App/Scripts/Management/RoundManager.cs
+++ Assets/App/Scripts/Management/RoundManager.cs
@@ -1,51 +1,47 @@
 using System.Collections.Generic;
 using UnityEngine;
+
 public class RoundManager : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] bool modeDev;
-
-    [Header("References")]
-
-    //[Space(10)]
-    // RSO
-    [SerializeField] RSO_BlobInGame rsoBlobInGame;
-    List<BlobMotor> blobs;
-    // RSF
-    // RSP
+    [SerializeField] private bool modeDev;
 
     [Header("Input")]
-    [SerializeField] RSE_OnBlobDeath rseOnBlobDeath;
-    [SerializeField] RSE_OnFightStart rseOnFightStart;
+    [SerializeField] private RSE_OnBlobDeath rseOnBlobDeath;
+    [SerializeField] private RSE_OnFightStart rseOnFightStart;
     [SerializeField] private RSE_Message rseMessage;
 
     [Header("Output")]
-    [SerializeField] RSE_OnFightEnd rseOnFightEnd;
-    [SerializeField] RSE_AddScore rseAddScore;
+    [SerializeField] private RSO_BlobInGame rsoBlobInGame;
+    [SerializeField] private RSE_OnFightEnd rseOnFightEnd;
+    [SerializeField] private RSE_AddScore rseAddScore;
+
+    private List<BlobMotor> blobs = new();
 
     private void OnEnable()
     {
         rseOnBlobDeath.action += CheckBlobCount;
         rseOnFightStart.action += Setup;
     }
+
     private void OnDisable()
     {
         rseOnBlobDeath.action -= CheckBlobCount;
         rseOnFightStart.action -= Setup;
     }
 
-    void Setup()
+    private void Setup()
     {
-        blobs = new List<BlobMotor>(rsoBlobInGame.Value);
+        blobs = rsoBlobInGame.Value;
     }
 
-    void CheckBlobCount(BlobMotor blob)
+    private void CheckBlobCount(BlobMotor blob)
     {
-        if(blobs != null && blobs.Contains(blob))
+        if (blobs != null && blobs.Contains(blob))
         {
             blobs.Remove(blob);
 
-            if (modeDev)
+            if (modeDev || blobs.Count == 0)
             {
                 rseOnFightEnd.Call();
                 rseMessage.Call("BLOB ??? WIN!", 1f, Color.black);
@@ -55,11 +51,6 @@ public class RoundManager : MonoBehaviour
                 rseAddScore.Call(blobs[0]);
                 rseOnFightEnd.Call();
                 rseMessage.Call($"BLOB {blobs[0].GetColor().nameColor} WIN!", 1f, blobs[0].GetColor().fillColor);
-            }
-            else if (blobs.Count < 1)
-            {
-                rseOnFightEnd.Call();
-                rseMessage.Call("BLOB ??? WIN!", 1f, Color.black);
             }
         }
     }
